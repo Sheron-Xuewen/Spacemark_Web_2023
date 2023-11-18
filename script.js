f7384c8e9ad25587fbca1c8188471e36dc2a389d
Sheron-Xuewen <144528382+Sheron-Xuewen@users.noreply.github.com>
2023-11-18 17:40:30 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const carets = document.querySelectorAll('.caret');
+    const subSections = document.querySelectorAll('.sub-section');
+    const menuIcon = document.getElementById('menu-icon');
+    const leftSidebar = document.getElementById('left-sidebar');
+    const backgroundLayer = document.getElementById('background-layer');
+
+    // Initialize sub-sections to be visible
+    subSections.forEach(subSection => {
+        subSection.style.display = 'block';
+    });
+
+    // Initialize carets to be downward
+    carets.forEach(caret => {
+        caret.innerHTML = '&#9660;'; // Downwards arrow
+    });
+
+    carets.forEach(caret => {
+        caret.addEventListener('click', function () {
+            const subSection = this.parentElement.nextElementSibling;
+            if (subSection.style.display === 'none' || subSection.style.display === '') {
+                subSection.style.display = 'block';
+                this.innerHTML = '&#9660;'; // Downwards arrow
+            } else {
+                subSection.style.display = 'none';
+                this.innerHTML = '&#9654;'; // Rightwards arrow
+            }
+        });
+    });
+
+    menuIcon.addEventListener('click', () => {
+        if (leftSidebar.style.display === 'none' || leftSidebar.style.display === '') {
+            leftSidebar.style.display = 'block';
+            backgroundLayer.style.display = 'block';
+        } else {
+            leftSidebar.style.display = 'none';
+            backgroundLayer.style.display = 'none';
+        }
+    });
+
+    // Function to handle responsiveness
+    function handleResponsiveness() {
+        const screenWidth = window.innerWidth;
+
+        if (screenWidth > 768) {
+            leftSidebar.style.display = 'block';
+            backgroundLayer.style.display = 'block';
+            menuIcon.style.display = 'none';
+        } else {
+            leftSidebar.style.display = 'none';
+            backgroundLayer.style.display = 'none';
+            menuIcon.style.display = 'block';
+        }
+    }
+
+    // Listen for window resize events
+    window.addEventListener('resize', handleResponsiveness);
+
+    // Call the function initially to set the correct state
+    handleResponsiveness();
+
+    document.addEventListener('click', function(event) {
+        const screenWidth = window.innerWidth;
+        if (screenWidth <= 768 && leftSidebar.style.display === 'block' && !leftSidebar.contains(event.target) && !menuIcon.contains(event.target)) {
+            leftSidebar.style.display = 'none';
+            backgroundLayer.style.display = 'none';
+        }
+    });
+    
+
+});
